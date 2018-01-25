2cbc25e6e409f9104d3ed98c61553a380790d7a6
Alexander Grimm <alexgrimm87@gmail.com>
2018-01-25 23:51:59 +0200

    commit

---
+++ build/js/develop/develop_7.js
@@ -0,0 +1,15 @@
+
+
+
+$(document).ready(function(){
+  $("#phone").mask("+7(999) 999-9999");
+  $("#card").mask("999-999");
+});
+
+$(window).load(function(){
+
+});
+
+$(window).resize(function(){
+
+});
